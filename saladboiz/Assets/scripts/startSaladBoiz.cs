3c321f9bdda82f6702fae3459fa9832ea7e09204
Llamas <llamaz@win.rpi.edu>
2019-04-08 07:46:45 -0400

    init scenes

---
+++ saladboiz/Assets/scripts/startSaladBoiz.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class startSaladBoiz : MonoBehaviour
+{
+    //objects
+    public GameObject about_info;
+    public GameObject about_button;
+    public GameObject restart_button;
+    public Sprite about_icon; // set to sprite that shows '?'
+    public Sprite about_x; // set to sprite that shows 'x'
+
+    //FUNCTIONS
+    public void ToggleAbout()
+    {
+        //toggle info section via button press
+        about_info.SetActive(!about_info.activeSelf);
+
+        //toggle about button display (open / close)
+        if (about_info.activeSelf) // set icon to 'x' b/c info is showing
+        {
+            about_button.GetComponent<Image>().sprite = about_x;
+        }
+        else
+        {
+            about_button.GetComponent<Image>().sprite = about_icon;
+        }
+    }
+
+    public void EnterCity()
+    {
+        SceneManager.LoadScene("city_scene");
+        restart_button.SetActive(!restart_button.activeSelf);
+    }
+
+    public void beingKind()
+    {
+        restart_button.SetActive(!restart_button.activeSelf);
+        SceneManager.LoadScene("opening_scene");
+    }
+}
